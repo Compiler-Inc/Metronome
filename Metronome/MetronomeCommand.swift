53d2a7dbd5216b330777abf1e27ce06cc2a5e49b
Taylor Holliday <wtholliday@gmail.com>
2025-01-04 17:18:15 -0800

    move code

---
+++ Metronome/MetronomeCommand.swift
@@ -0,0 +1,42 @@
+
+enum MetronomeCommand {
+    case play
+    case stop
+    case setTempo(bpm: Double)
+    case rampTempo(bpm: Double, duration: Double)
+    case changeSound(sound: String)
+    case setDownBeat(sound: String)
+    case setUpBeat(sound: String)
+    case setGapMeasures(count: Int)
+    case noOp
+
+    static func from(_ command: DLMCommand) -> MetronomeCommand? {
+        switch command.command {
+        case "play":
+            return .play
+        case "stop":
+            return .stop
+        case "setTempo":
+            guard let bpm = command.args?.bpm else { return nil }
+            return .setTempo(bpm: bpm)
+        case "rampTempo":
+            guard let bpm = command.args?.bpm else { return nil }
+            guard let duration = command.args?.duration else { return nil }
+            return .rampTempo(bpm: bpm, duration: duration)
+        case "changeSound":
+            guard let sound = command.args?.sound else { return nil }
+            return .changeSound(sound: sound)
+        case "setDownBeat":
+            guard let sound = command.args?.sound else { return nil }
+            return .setDownBeat(sound: sound)
+        case "setUpBeat":
+            guard let sound = command.args?.sound else { return nil }
+            return .setUpBeat(sound: sound)
+        case "setGapMeasures":
+            guard let count = command.args?.count else { return nil }
+            return .setGapMeasures(count: count)
+        default:
+            return nil
+        }
+    }
+}
