aef7921cce5ba89a61b9ad2bd9805b588260285d
Aurelius Prochazka <aure@aure.com>
2025-01-06 15:17:49 -0800

    work on DLMViewModel

--- Metronome/DLMViewModel.swift
+++ Metronome/DLMViewModel.swift
@@ -6,9 +6,15 @@
 //
 
 import Foundation
+import AudioKit
 
+@MainActor
 @Observable
 class DLMViewModel {
+    
+    let deepgram = DeepgramService()
+    let audioEngine = AudioEngine()
+    private var promptTap: RawDataTap?
     
     var processingSteps: [ProcessingStep] = []
     
@@ -20,6 +26,36 @@ class DLMViewModel {
         if let lastIndex = processingSteps.indices.last {
             processingSteps[lastIndex].isComplete = true
         }
+    }
+    
+    func startRealtimeTranscription() {
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("Error starting audio engine: \(error)")
+            return
+        }
+        
+        guard let input = audioEngine.input else {
+            print("No input!")
+            return
+        }
+        
+        promptTap = RawDataTap(input, bufferSize: 4096) { buffer in
+            self.deepgram.sendAudioData(buffer)
+        }
+        promptTap?.start()
+        
+        // Start WebSocket connection
+        deepgram.startRealtimeTranscription()
+    }
+    
+    func stopRealtimeTranscription() {
+        promptTap?.stop()
+        promptTap = nil
+        deepgram.stopRealtimeTranscription()
+        audioEngine.stop()
     }
     
 }
