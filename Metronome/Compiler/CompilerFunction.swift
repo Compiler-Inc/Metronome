b7f389ffcac51f2d4476aa1624255c6dd3a04ec5
Aurelius Prochazka <aure@aure.com>
2025-03-18 14:55:14 -0700

    Removed docu-comments that were not parsed

--- Metronome/Compiler/CompilerFunction.swift
+++ Metronome/Compiler/CompilerFunction.swift
@@ -3,7 +3,6 @@
 import Foundation
 import CompilerSwiftAI
 
-/// Compiler Function
 enum CompilerFunction: Decodable, Sendable {
     private enum CodingKeys: String, CodingKey {
         case function
@@ -14,92 +13,64 @@ enum CompilerFunction: Decodable, Sendable {
     private enum FunctionType: String, Decodable {
         case play
         case stop
-        case setTempo = "setTempo"
-        case rampTempo = "rampTempo"
-        case changeSound = "changeSound"
-        case setDownBeat = "setDownBeat"
-        case setUpBeat = "setUpBeat"
-        case setGapMeasures = "setGapMeasures"
-        case setNumberOfBeats = "setNumberOfBeats"
-        case setColor = "setColor"
-        case noOp = "noOp"
+        case setTempo
+        case rampTempo
+        case changeSound
+        case setDownBeat
+        case setUpBeat
+        case setGapMeasures
+        case setNumberOfBeats
+        case setColor
+        case noOp
     }
     
-    /// Make the metronome start making sounds
     struct PlayParameters: Decodable, Sendable {}
     case play(Function<PlayParameters>)
     
-    /// Make the metronome no longer play sounds
     struct StopParameters: Decodable, Sendable {}
     case stop(Function<StopParameters>)
-    
-    /// Set the tempo or speed of the metronome in "beats per minute (bpm)"
-    /// - Parameters:
-    ///   - bpm: The target tempo in beats per minute, need to be between 20 and 300
+
     struct SetTempoParameters: Decodable, Sendable {
         let bpm: Double
     }
     case setTempo(Function<SetTempoParameters>)
-    
-    /// Increase or decrease the tempo of the metronome over a specified duration
-    /// - Parameters:
-    ///   - bpm: The target tempo in beats per minute, need to be between 20 and 300
-    ///   - duration: The length of time in seconds it will take to reach the target tempo
+
     struct RampTempoParameters: Decodable, Sendable {
         let bpm: Double
         let duration: Double
     }
     case rampTempo(Function<RampTempoParameters>)
     
-    /// Change the primary sound of the metronome, which can be referred to as the pulse
-    /// - Parameters:
-    ///   - sound: This sound has to be one of the following: snap, knock, Acoustic Bass Drum, Bass Drum 1, Side Stick, Acoustic Snare, Hand Clap, Electric Snare, Low Floor Tom, Closed Hi Hat, High Floor Tom, Pedal Hi-Hat, Low Tom, Open Hi-Hat, Low-Mid Tom, Hi Mid Tom, Crash Cymbal 1, High Tom, Ride Cymbal 1, Chinese Cymbal, Ride Bell, Tambourine, Splash Cymbal, Cowbell, Crash Cymbal 2, Vibraslap, Ride Cymbal 2, Hi Bongo, Low Bongo, Mute Hi Conga, Open Hi Conga, Low Conga, High Timbale, Low Timbale, High Agogo, Low Agogo, Cabasa, Maracas, Short Guiro, Long Guiro, Claves, Hi Wood Block, Low Wood Block, Mute Triangle, Open Triangle
     struct ChangeSoundParameters: Decodable, Sendable {
         let sound: String
     }
     case changeSound(Function<ChangeSoundParameters>)
     
-    /// Change the downbeat sound of the metronome, which can be referred to as the first beat sound
-    /// - Parameters:
-    ///   - sound: This sound has to be one of the following: snap, knock, Acoustic Bass Drum, Bass Drum 1, Side Stick, Acoustic Snare, Hand Clap, Electric Snare, Low Floor Tom, Closed Hi Hat, High Floor Tom, Pedal Hi-Hat, Low Tom, Open Hi-Hat, Low-Mid Tom, Hi Mid Tom, Crash Cymbal 1, High Tom, Ride Cymbal 1, Chinese Cymbal, Ride Bell, Tambourine, Splash Cymbal, Cowbell, Crash Cymbal 2, Vibraslap, Ride Cymbal 2, Hi Bongo, Low Bongo, Mute Hi Conga, Open Hi Conga, Low Conga, High Timbale, Low Timbale, High Agogo, Low Agogo, Cabasa, Maracas, Short Guiro, Long Guiro, Claves, Hi Wood Block, Low Wood Block, Mute Triangle, Open Triangle
     struct SetDownBeatParameters: Decodable, Sendable {
         let sound: String
     }
     case setDownBeat(Function<SetDownBeatParameters>)
     
-    /// Change the upbeat sound of the metronome, which can be referred to as the accent
-    /// - Parameters:
-    ///   - sound: This sound has to be one of the following: snap, knock, Acoustic Bass Drum, Bass Drum 1, Side Stick, Acoustic Snare, Hand Clap, Electric Snare, Low Floor Tom, Closed Hi Hat, High Floor Tom, Pedal Hi-Hat, Low Tom, Open Hi-Hat, Low-Mid Tom, Hi Mid Tom, Crash Cymbal 1, High Tom, Ride Cymbal 1, Chinese Cymbal, Ride Bell, Tambourine, Splash Cymbal, Cowbell, Crash Cymbal 2, Vibraslap, Ride Cymbal 2, Hi Bongo, Low Bongo, Mute Hi Conga, Open Hi Conga, Low Conga, High Timbale, Low Timbale, High Agogo, Low Agogo, Cabasa, Maracas, Short Guiro, Long Guiro, Claves, Hi Wood Block, Low Wood Block, Mute Triangle, Open Triangle
     struct SetUpBeatParameters: Decodable, Sendable {
         let sound: String
     }
     case setUpBeat(Function<SetUpBeatParameters>)
     
-    /// After every measure of the metronome playing, we can play any number of silent measures
-    /// - Parameters:
-    ///   - count: The number of measures of silence to be played
     struct SetGapMeasureParameters: Decodable, Sendable {
         let count: Int
     }
     case setGapMeasures(Function<SetGapMeasureParameters>)
     
-    /// Sets the number of beats that are shown on the metronome. This is also the top number of a traiditional Time Signature, so the user may say something like "Set the time signature to 7 4" and you will only change the first or top number.
-    /// - Parameters:
-    ///   - numberOfBeats: The number of beats to show, also the top number of a traditional time signature
     struct SetNumberOfBeatsParameters: Decodable, Sendable {
         let numberOfBeats: Int
     }
     case setNumberOfBeats(Function<SetNumberOfBeatsParameters>)
     
-    /// Sets the color or style of the metronome. The available colors are "red", "orange", "yellow", "green", "mint", "teal", "cyan", "blue", "indigo", "purple", "pink", "brown", "black", "white", "gray"'. If the prompt is a color not listed, do your best to match it with a corresponding color. For example "make the style Crimson" should match with "red".
-    /// - Parameters:
-    ///   - color: The color to set
     struct SetColorParameters: Decodable, Sendable {
         let color: String
     }
     case setColor(Function<SetColorParameters>)
     
-    /// This is returned if the function could not be converted into appropriate functions
     struct NoOpParameters: Decodable, Sendable {}
     case noOp(Function<NoOpParameters>)
     
