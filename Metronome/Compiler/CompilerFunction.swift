de058ab500fca483cd7f58067fc754ea37f592a1
Aurelius Prochazka <aure@aure.com>
2025-03-20 16:36:17 -0700

    Using CompilerHelper package for CompilerFunction generator macro

--- Metronome/Compiler/CompilerFunction.swift
+++ Metronome/Compiler/CompilerFunction.swift
@@ -1,143 +1,56 @@
 //  Copyright 2025 Compiler, Inc. All rights reserved.
 
-import Foundation
+import CompilerHelper
 import CompilerSwiftAI
 
-enum CompilerFunction: Decodable, Sendable {
-    private enum CodingKeys: String, CodingKey {
-        case function
-        case parameters
-        case colloquialResponse = "colloquial_response"
-    }
+@CompilerFunctionGenerator
+enum CompilerFunctionDef {
+    /// Make the metronome start making sounds
+    case play
     
-    private enum FunctionType: String, Decodable {
-        case play
-        case stop
-        case setTempo
-        case rampTempo
-        case changeSound
-        case setDownBeat
-        case setUpBeat
-        case setGapMeasures
-        case setNumberOfBeats
-        case setColor
-        case noOp
-    }
+    /// Make the metronome no longer play sounds
+    case stop
     
-    struct PlayParameters: Decodable, Sendable {}
-    case play(Function<PlayParameters>)
+    /// Set the tempo or speed of the metronome iin "beats per minute (bpm)"
+    /// - Parameters:
+    ///   - bpm: The target tempo in beats per minute, need to be between 20 and 300
+    case setTempo(bpm: Double)
     
-    struct StopParameters: Decodable, Sendable {}
-    case stop(Function<StopParameters>)
-
-    struct SetTempoParameters: Decodable, Sendable {
-        let bpm: Double
-    }
-    case setTempo(Function<SetTempoParameters>)
-
-    struct RampTempoParameters: Decodable, Sendable {
-        let bpm: Double
-        let duration: Double
-    }
-    case rampTempo(Function<RampTempoParameters>)
+    /// Set the numerator in the time signature
+    /// - Parameters:
+    ///   - numberOfBeats: The number of beats in one measure
+    case setNumberOfBeats(numberOfBeats: Int)
     
-    struct ChangeSoundParameters: Decodable, Sendable {
-        let sound: String
-    }
-    case changeSound(Function<ChangeSoundParameters>)
+    /// Increase or decrease the tempo of the metronome over a specified duration,
+    /// - Parameters:
+    ///   - bpm: The target tempo in beats per minute, need to be between 20 and 300
+    ///   - duration: The length of time in seconds it will take to reach the target tempo
+    case rampTempo(bpm: Double, duration: Double)
     
-    struct SetDownBeatParameters: Decodable, Sendable {
-        let sound: String
-    }
-    case setDownBeat(Function<SetDownBeatParameters>)
+    /// Change the primary sound of the metronome, which can be referred to as the pulse
+    /// - Parameters:
+    ///   - sound: This sound has to be one of the following: snap, knock, Acoustic Bass Drum, Bass Drum 1, Side Stick, Acoustic Snare, Hand Clap, Electric Snare, Low Floor Tom, Closed Hi Hat, High Floor Tom, Pedal Hi-Hat, Low Tom, Open Hi-Hat, Low-Mid Tom, Hi Mid Tom, Crash Cymbal 1, High Tom, Ride Cymbal 1, Chinese Cymbal, Ride Bell, Tambourine, Splash Cymbal, Cowbell, Crash Cymbal 2, Vibraslap, Ride Cymbal 2, Hi Bongo, Low Bongo, Mute Hi Conga, Open Hi Conga, Low Conga, High Timbale, Low Timbale, High Agogo, Low Agogo, Cabasa, Maracas, Short Guiro, Long Guiro, Claves, Hi Wood Block, Low Wood Block, Mute Triangle, Open Triangle
+    case changeSound(sound: String)
     
-    struct SetUpBeatParameters: Decodable, Sendable {
-        let sound: String
-    }
-    case setUpBeat(Function<SetUpBeatParameters>)
+    /// Change the downbeat sound of the metronome, which can be referred to as the first beat sound
+    /// - Parameters:
+    ///   - sound: This sound has to be one of the following: snap, knock, Acoustic Bass Drum, Bass Drum 1, Side Stick, Acoustic Snare, Hand Clap, Electric Snare, Low Floor Tom, Closed Hi Hat, High Floor Tom, Pedal Hi-Hat, Low Tom, Open Hi-Hat, Low-Mid Tom, Hi Mid Tom, Crash Cymbal 1, High Tom, Ride Cymbal 1, Chinese Cymbal, Ride Bell, Tambourine, Splash Cymbal, Cowbell, Crash Cymbal 2, Vibraslap, Ride Cymbal 2, Hi Bongo, Low Bongo, Mute Hi Conga, Open Hi Conga, Low Conga, High Timbale, Low Timbale, High Agogo, Low Agogo, Cabasa, Maracas, Short Guiro, Long Guiro, Claves, Hi Wood Block, Low Wood Block, Mute Triangle, Open Triangle
+    case setDownBeat(sound: String)
     
-    struct SetGapMeasureParameters: Decodable, Sendable {
-        let count: Int
-    }
-    case setGapMeasures(Function<SetGapMeasureParameters>)
+    /// Change the upbeat sound of the metronome, which can be referred to as the accent
+    /// - Parameters:
+    ///   - sound: This sound has to be one of the following: snap, knock, Acoustic Bass Drum, Bass Drum 1, Side Stick, Acoustic Snare, Hand Clap, Electric Snare, Low Floor Tom, Closed Hi Hat, High Floor Tom, Pedal Hi-Hat, Low Tom, Open Hi-Hat, Low-Mid Tom, Hi Mid Tom, Crash Cymbal 1, High Tom, Ride Cymbal 1, Chinese Cymbal, Ride Bell, Tambourine, Splash Cymbal, Cowbell, Crash Cymbal 2, Vibraslap, Ride Cymbal 2, Hi Bongo, Low Bongo, Mute Hi Conga, Open Hi Conga, Low Conga, High Timbale, Low Timbale, High Agogo, Low Agogo, Cabasa, Maracas, Short Guiro, Long Guiro, Claves, Hi Wood Block, Low Wood Block, Mute Triangle, Open Triangle
+    case setUpBeat(sound: String)
     
-    struct SetNumberOfBeatsParameters: Decodable, Sendable {
-        let numberOfBeats: Int
-    }
-    case setNumberOfBeats(Function<SetNumberOfBeatsParameters>)
+    /// After every measure of the metronome playing, we can play any number of silent measures
+    /// - Parameters:
+    ///   - count: The number of measures of silence to be played
+    case setGapMeasures(count: Int)
     
-    struct SetColorParameters: Decodable, Sendable {
-        let color: String
-    }
-    case setColor(Function<SetColorParameters>)
-    
-    struct NoOpParameters: Decodable, Sendable {}
-    case noOp(Function<NoOpParameters>)
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        let functionType = try container.decode(FunctionType.self, forKey: .function)
-        let colloquialResp = try container.decodeIfPresent(String.self, forKey: .colloquialResponse) ?? "Processing metronome command"
-        
-        switch functionType {
-            case .play:
-                self = .play(.init(id: functionType.rawValue, parameters: PlayParameters(), colloquialResponse: colloquialResp))
-            case .stop:
-                self = .stop(.init(id: functionType.rawValue, parameters: StopParameters(), colloquialResponse: colloquialResp))
-            case .setTempo:
-                let params = try container.decodeIfPresent(SetTempoParameters.self, forKey: .parameters) ?? nil
-                self = .setTempo(.init(id: functionType.rawValue, parameters: params, colloquialResponse: colloquialResp))
-            case .rampTempo:
-                let params = try container.decodeIfPresent(RampTempoParameters.self, forKey: .parameters) ?? nil
-                self = .rampTempo(.init(id: functionType.rawValue, parameters: params, colloquialResponse: colloquialResp))
-            case .changeSound:
-                let params = try container.decodeIfPresent(ChangeSoundParameters.self, forKey: .parameters) ?? nil
-                self = .changeSound(.init(id: functionType.rawValue, parameters: params, colloquialResponse: colloquialResp))
-            case .setDownBeat:
-                let params = try container.decodeIfPresent(SetDownBeatParameters.self, forKey: .parameters) ?? nil
-                self = .setDownBeat(.init(id: functionType.rawValue, parameters: params, colloquialResponse: colloquialResp))
-            case .setUpBeat:
-                let params = try container.decodeIfPresent(SetUpBeatParameters.self, forKey: .parameters) ?? nil
-                self = .setUpBeat(.init(id: functionType.rawValue, parameters: params, colloquialResponse: colloquialResp))
-            case .setGapMeasures:
-                let params = try container.decodeIfPresent(SetGapMeasureParameters.self, forKey: .parameters) ?? nil
-                self = .setGapMeasures(.init(id: functionType.rawValue, parameters: params, colloquialResponse: colloquialResp))
-            case .setNumberOfBeats:
-                let params = try container.decodeIfPresent(SetNumberOfBeatsParameters.self, forKey: .parameters) ?? nil
-                self = .setNumberOfBeats(.init(id: functionType.rawValue, parameters: params, colloquialResponse: colloquialResp))
-            case .setColor:
-                let params = try container.decodeIfPresent(SetColorParameters.self, forKey: .parameters) ?? nil
-                self = .setColor(.init(id: functionType.rawValue, parameters: params, colloquialResponse: colloquialResp))
-            case .noOp:
-                let params = try container.decodeIfPresent(NoOpParameters.self, forKey: .parameters) ?? nil
-                self = .noOp(.init(id: functionType.rawValue, parameters: params, colloquialResponse: colloquialResp))
-        }
-    }
-    
-    var colloquialResponse: String {
-        switch self {
-            case .play(let function):
-                return function.colloquialResponse
-            case .stop(let function):
-                return function.colloquialResponse
-            case .setTempo(let function):
-                return function.colloquialResponse
-            case .rampTempo(let function):
-                return function.colloquialResponse
-            case .changeSound(let function):
-                return function.colloquialResponse
-            case .setDownBeat(let function):
-                return function.colloquialResponse
-            case .setUpBeat(let function):
-                return function.colloquialResponse
-            case .setGapMeasures(let function):
-                return function.colloquialResponse
-            case .noOp(let function):
-                return function.colloquialResponse
-            case .setNumberOfBeats(let function):
-                return function.colloquialResponse
-            case .setColor(let function):
-                return function.colloquialResponse
-        }
-    }
+    /// Set the primary color used in the user interface
+    /// - Parameters:
+    ///   - color: The tint of the UI
+    case setColor(color: String)
+    case noOp
 }
+
